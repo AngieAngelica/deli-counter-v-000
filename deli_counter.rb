daadc2288f275b3aa47613d4be7436b258d1b929
Angie Kassab <anasab12@gmail.com>
2018-07-17 21:11:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,10 +11,10 @@ def line(katz_deli)
 end
 
 
-# def take_a_number(katz_deli, name)
-#   puts "Welcome, #{name}. You are number #{katz_deli.count + 1} in line."
-#   katz_deli.push(name)
-# end
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count + 1} in line."
+  katz_deli.push(name)
+end
 
 def now_serving (katz_deli)
   if katz_deli.empty? == true
