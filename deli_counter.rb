ed586034a56becc8855bbc65b52588207b4958ea
Angie Kassab <anasab12@gmail.com>
2018-07-17 21:05:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,15 @@
 katz_deli = []
 
-def line
-  
+def line(katz_deli)
+ if katz_deli.empty? == false
+    katz_deli.each_with_index do|value,index|
+     puts "The line is currently: #{index+1}. #{value}"
+ end
+ elsif katz_deli.empty? == true
+     puts "The line is currently empty."
+ end
 end
+
 
 def take_a_number(argument, argument2)
 end
