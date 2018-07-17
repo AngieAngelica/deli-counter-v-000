a1fba696c2a6ea7dde3b9db3681049a09faaeded
Angie Kassab <anasab12@gmail.com>
2018-07-17 21:03:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line
+  
+end
+
+def take_a_number(argument, argument2)
+end
+
+def now_serving (katz_deli)
+  if katz_deli.empty? == true
+    puts"There is nobody waiting to be served!"
+  elsif katz_deli.empty? == false
+
+    puts"Currently serving #{katz_deli.first}."
+  katz_deli.shift
+  end
+end
+
+
