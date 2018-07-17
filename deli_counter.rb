693be9243f5b27ae2b857516b7ddbade23ffe3e2
Angie Kassab <anasab12@gmail.com>
2018-07-17 21:31:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(katz_deli)
   if katz_deli.empty? == true
      puts "The line is currently empty."
   else
-    katz_deli.each_with_index do |name, index|
+    line_number = 0
+    katz_deli.each do |name|
      puts "The line is currently: #{index + 1}. #{name}"
   end
   end
