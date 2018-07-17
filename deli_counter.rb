cc0a019dc029bbfd061e601d79849b406041e71a
Angie Kassab <anasab12@gmail.com>
2018-07-17 21:09:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,10 +18,6 @@ def take_a_number(katz_deli, name)
   return name, position
 end
 
-
-def take_a_number(argument, argument2)
-end
-
 def now_serving (katz_deli)
   if katz_deli.empty? == true
     puts"There is nobody waiting to be served!"
